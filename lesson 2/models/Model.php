f38b400b187be4afd85fdc65237c392e37221a2c
devmindsru <devminds@mail.ru>
2019-02-12 10:28:06 +0300

    dz 2

---
+++ lesson 2/models/Model.php
@@ -0,0 +1,14 @@
+<?php
+namespace demidielon\php2\models;
+use \demidielon\php2\interfaces\IModel;
+/**
+* Model
+*/
+abstract class Model implements IModel
+{
+  function __construct()
+  {
+    # code...
+  }
+}
+?>
