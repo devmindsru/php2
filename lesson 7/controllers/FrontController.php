b6e3da6d0db3cbe6df891e2cec8b7323120003d9
devmindsru <devminds@mail.ru>
2019-03-01 11:09:48 +0300

    hw7

---
+++ lesson 7/controllers/FrontController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace app\controllers;
+
+use app\base\App;
+use app\models\User;
+
+use app\services\RequestNotMatchException;
+
+use app\services\AutoloaderNotMatchException;
+
+class FrontController extends Controller
+{
+    private $controller;
+    private $action;
+
+    private $defaultController = "Product";
+
+    public function actionIndex()
+    {
+        // Обращение к компоненту Request по имени, а не через new Request(), как раньше
+        try {
+            $rm = App::call()->request;
+        } catch (RequestNotMatchException $e) {
+            $this->redirect();
+        }
+
+        $controllerName = $rm->getControllerName() ?: $this->defaultController;
+        $this->action = $rm->getActionName();
+
+        $this->controller = App::call()->config['controller_namespaces']
+            . ucfirst($controllerName) . "Controller";
+        $this->checkLogin();
+
+        // Ловим несуществующий Controller
+        try {
+            $controller = new $this->controller(
+                new \app\services\renderers\TemplateRenderer()
+            );
+        } catch (AutoloaderNotMatchException $e) {
+            $this->redirect();
+        }
+
+        // Ловим несуществующий Action
+        try {
+            $controller->runAction($this->action);
+        } catch (ActionNotMatchException $e) {
+            $this->redirect();
+        }
+
+    }
+
+    // Если пользователь авторизован, пускаем дальше, если нет - отправляем на страницу авторизации
+    private function checkLogin()
+    {
+        // Используем сессии, чтобы при каждом заходе пользователя не спрашивать его про авторизацию
+        session_start();
+        // Чтобы не было бесконечного редиректа добавляем условие
+        if ($this->controller != "\\" . AuthController::class) {
+            // Успешно получен ли текущий пользователь
+            $user = (new User())->getCurrent();
+            // Отправляем на страницу авторизации
+            if (!$user) {
+                $this->redirect();
+            }
+        }
+    }
+}
